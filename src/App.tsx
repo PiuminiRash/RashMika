8fdcc6998fbb3d8db2399ddc070b771013d55d4c
PiuminiRash <piuminirashmika@gmail.com>
2025-12-22 23:25:21 +0530

    folder update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { useEffect, useState } from "react";
 import RootLayout from "./components/RootLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Works from "./pages/Works";
 import Contact from "./pages/Contact";
 import Loader from "./components/Loader";
+import { useEffect, useState } from "react";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "works", element: <Works /> },
+        { path: "contact", element: <Contact /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "home", element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "works", element: <Works /> },
-      { path: "contact", element: <Contact /> },
-    ],
-  },
-]);
+    basename: "/RashMika", // 👈 REQUIRED
+  }
+);
 
-export default function App() {
+function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
-    return () => clearTimeout(timer);
+    setTimeout(() => setLoading(false), 3000);
   }, []);
 
   return loading ? <Loader /> : <RouterProvider router={router} />;
 }
+
+export default App;
