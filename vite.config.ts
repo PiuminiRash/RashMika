b5f01fc6ded3f7ad93afe09c0263a292d1144eae
PiuminiRash <piuminirashmika@gmail.com>
2025-12-23 19:08:36 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/RashMika/',   // 🔴 REQUIRED (repo name)
   plugins: [react()],
-  build: {
-    outDir: 'build',
-    target: 'es2019'
-  }
-});
+  base: '/RashMika/',   // 👈 VERY IMPORTANT
+})
